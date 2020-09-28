482da66aef7c6e9281783d228bba905dda43545c
Evgeny <zhenya.shnayder.87@mail.ru>
2020-09-28 12:19:48 +0700

    .

--- src/pug/includes/Inputs/Inputs-with-counter/Inputs-with-counter.js
+++ src/pug/includes/Inputs/Inputs-with-counter/Inputs-with-counter.js
@@ -1,63 +1,108 @@
-let fc = false
-function inputfocus(item){
-    item.classList.add("WithDropdownactive");
-
-};
-function inputnotfocus(item){
-    item.classList.remove("WithDropdownactive");
-    
-};
-
-
-function jsUcfirst(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 export default function(){
-    let inputs = document.querySelectorAll('.WithDropdown');
-
-    inputs.forEach(item=>{
-        item.addEventListener('focus', e=> {fc = !fc; inputfocus(e.target)} );
-        item.addEventListener('blur', e=>{fc = !fc; inputnotfocus(e.target)} );
-        item.addEventListener('mouseover', e=> {if(!fc) inputfocus(e.target)} );
-        item.addEventListener('mouseout', e=>{if(!fc) inputnotfocus(e.target)} );
-    });
+    document.querySelectorAll(".Inputs-with-counter_textfield").forEach(par=>{
+        let inp = par.querySelector('input');
+        let list = par.querySelector('.Inputs-with-counter-menu');
+        let listEl = list.querySelectorAll('li');
+        let sumof = false
 
 
-    document.querySelectorAll('.WithDropdown').forEach(inp =>{
-        inp.addEventListener('focus', e =>{
-            let drop = e.target.parentElement.nextElementSibling;
-            let dropch = drop.querySelectorAll('div')
+        conf();
+        listEl.forEach(li=>{
+            let counter = li.querySelector('.Inputs-with-counter-counter')
+            let numb = counter.querySelector(".Inputs-with-counter-number")
+            let min = +(counter.getAttribute('data-min') )
+            let max = +(counter.getAttribute('data-max') )
+            if (+numb.textContent == min){
+                depr(counter.firstChild)
+            };
+            if (+numb.textContent == max){
+                depr(counter.lastChild)
+            }
 
-            dropch.forEach(ch=>{
-                ch.style.display = 'block';
-                ch.addEventListener('click', ev=>{
-                    e.target.value = ev.target.closest('div').textContent;
-                    dropch.forEach(dropch =>{
-                        dropch.style.display = 'none'
-                    })
-                })
+            counter.firstChild.addEventListener('click', evmin)
+            counter.lastChild.addEventListener('click', evplus)
+
+            function evplus(){
+                let inc = Math.min(+numb.textContent+1, max)
+                numb.textContent = inc;
+                if (inc == max){depr(counter.lastChild)};
+                par.querySelector('.Inputs-with-counter_delete').style.display="inline-block";
+                apr(counter.firstChild)
+            }
+            function evmin(){
+                let inc = Math.max(+numb.textContent-1, min)
+                numb.textContent = inc
+                if (inc == min){depr(counter.firstChild)};
+                apr(counter.lastChild);
+            }
+
+
+            sumof= Boolean(+numb.textContent)
+            if(!sumof){par.querySelector('.Inputs-with-counter_delete').style.display="none"}
+        })
+
+       
+        inp.addEventListener('focus', e=>{
+            list.style.display = "flex";
+            inp.classList.add('WithDropdownactive')
+            document.addEventListener('click',function evcl(ev){
+                if (!par.contains(ev.target) ){ 
+                    conf();
+                    inp.classList.remove('WithDropdownactive')
+                    list.style.display = "none" 
+                    }
             })
+
         })
-        inp.addEventListener('blur', e =>{
-            let drop = e.target.parentElement.nextElementSibling;
-            let dropch = drop.querySelectorAll('div');
-            setTimeout(()=>{
-                dropch.forEach(dropch =>{
-                dropch.style.display = 'none'
-                })
-            }, 100);
+
+        par.querySelector('.Inputs-with-counter_delete').addEventListener('click', e=>{
+            del()
         })
-        inp.addEventListener('input', e=>{
-            let drop = e.target.parentElement.nextElementSibling;
-            let dropch = drop.querySelectorAll('div');
-            inp.value = jsUcfirst(inp.value)
-            dropch.forEach(e=>{
-                if(e.innerHTML.includes(inp.value) ){e.style.display = "block"}
-                else{e.style.display = "none"}
+        par.querySelector('.Inputs-with-counter_confirm').addEventListener('click',e=>{
+            conf()
+            list.style.display = "none" ;
+        })
+        
+        
+        function conf(){
+            let cont = ''
+            listEl.forEach( (it,i) =>{
+                
+                let num = +it.querySelector(".Inputs-with-counter-number").textContent
+                let word = it.querySelector('p').textContent
+                if(i==2){cont+='...'}
+                else {cont+= `${num} ${word}, `}
+    
             })
-        })
+            inp.classList.remove('WithDropdownactive')
+            inp.value = cont
+
+        }
+
+
+        function del(){
+            listEl.forEach(item=>{
+                let limit = +item.querySelector('.Inputs-with-counter-counter').getAttribute('date-min')
+                item.querySelector('.Inputs-with-counter-number').textContent = `${limit}`;
+                let f = item.querySelector('.Inputs-with-counter-counter').firstChild;
+                par.querySelector('.Inputs-with-counter_delete').style.display="none";
+                depr(f)
+                conf()
+            })
+        }
+        
+
+        function depr(ar){
+            ar.style.border= '1px solid rgba(31, 32, 65, 0.25)';
+            ar.style.color = 'rgba(31, 32, 65, 0.25)';
+            ar.style.cursor = "auto"
+        }
+        function apr(ar){
+            ar.style.border= '1px solid rgba(31, 32, 65, 0.5)';
+            ar.style.color = 'rgba(31, 32, 65, 0.5)';
+            ar.style.cursor = "pointer"
+        }
+
     })
-};
-
-
+}
--- src/pug/pages/FormElements/FormElements.js
+++ src/pug/pages/FormElements/FormElements.js
@@ -1,19 +1,21 @@
 import focusinput from '../../includes/Inputs/TextField/TextField.js'
 import withDropdown from '../../includes/Inputs/WithDropdown/WithDropdown.js'
-import Mask_d_m_y from "../../includes/Inputs/Mask_d-m-y/Mask_d-m-y.js"
-import Field_arrow from "../../includes/Inputs/Field-with-arrow/Field-with-arrow.js"
+import mask_d_m_y from "../../includes/Inputs/Mask_d-m-y/Mask_d-m-y.js"
+import field_arrow from "../../includes/Inputs/Field-with-arrow/Field-with-arrow.js"
 //import $ from 'jquery';
 //import 'bootstrap'
 import "../../plugins/jquery.maskinput.min.js"
-import Date_picker from "../../includes/Inputs/Date-picker/Date-picker.js"
-import Date_range from "../../includes/Inputs/Date-range/Date-range.js"
+import date_picker from "../../includes/Inputs/Date-picker/Date-picker.js"
+import date_range from "../../includes/Inputs/Date-range/Date-range.js"
+import input_counter from "../../includes/Inputs/Inputs-with-counter/Inputs-with-counter" 
 
 
 document.addEventListener('DOMContentLoaded', function(e){
     focusinput();
     withDropdown();
-    Mask_d_m_y();
-    Date_picker();
-    Date_range();
-    Field_arrow();
+    mask_d_m_y();
+    date_picker();
+    date_range();
+    field_arrow();
+    input_counter();
 })
