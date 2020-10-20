088d47e04542b55d8e10ce2e19d98e4c5f265d16
Evgeny <zhenya.shnayder.87@mail.ru>
2020-10-20 17:36:00 +0700

    UI was FINALLY done

---
+++ src/pug/includes/Blocks/Burger/Burger.js
@@ -0,0 +1,28 @@
+const BurgerFirstTimeCecking = true
+
+export default function(){
+    document.querySelectorAll(".Header__burger").forEach(it=>{
+        let menu = it.parentNode.querySelector(".Menu")
+        it.addEventListener("click", e=>{
+            it.classList.toggle("Header__burger_cond_active")
+            menu.style.visibility = getComputedStyle(menu).visibility == "hidden" ? "visible" : "hidden"
+            if(BurgerFirstTimeCecking){
+                
+                window.addEventListener('resize', ev=>{
+                    if(document.documentElement.clientWidth> 600){
+                        menu.style.visibility = "visible"
+                        try{it.classList.remove("Header__burger_cond_active")}
+                        catch{}
+                    }
+                    else if(document.documentElement.clientWidth<=600){
+                        menu.style.visibility = "hidden"
+                        try{it.classList.remove("Header__burger_cond_active")}
+                        catch{}
+                    }
+                })
+                BurgerFirstTimeCecking = false
+            }
+
+        })
+    })
+}
--- src/pug/includes/Blocks/Header/Header.js
+++ src/pug/includes/Blocks/Header/Header.js
@@ -1,5 +1,6 @@
-import Header__menu from  ".//Header__menu/Header__menu.js"
-
+import Header__menu from  "./Header__menu/Header__menu.js"
+import Burger from "../Burger/Burger.js"
 export default function(){
     Header__menu()
+    Burger()
 }
