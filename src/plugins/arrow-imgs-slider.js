7eadc9dc7b86ae952f026af5c67976600215c3b7
Evgeny <zhenya.shnayder.87@mail.ru>
2021-04-04 03:41:25 +0700

    fix

--- src/plugins/arrow-imgs-slider.js
+++ src/plugins/arrow-imgs-slider.js
@@ -1,74 +1,70 @@
-function arrowImgsSlider(data){
-    let slider = new ArrowImgsSlider(data)
-    slider.init()
-    return slider
+function arrowImgsSlider(data) {
+  const slider = new ArrowImgsSlider(data);
+  slider.init();
+  return slider;
 }
 
-class ArrowImgsSlider{
+class ArrowImgsSlider {
+  constructor({
+    arrowPrev, arrowNext, imgs, buttons, imgDisactiveClass, buttonDisactiveClass,
+  }) {
+    this.arrowPrev = arrowPrev;
+    this.arrowNext = arrowNext;
+    this.imags = imgs;
+    this.buttons = buttons;
+    this.disactImgClass = imgDisactiveClass;
+    this.disactBtnClass = buttonDisactiveClass;
+  }
 
-    constructor({arrowPrev , arrowNext, imgs, buttons, imgDisactiveClass, buttonDisactiveClass}){
-        this.arrowPrev = arrowPrev 
-        this.arrowNext = arrowNext 
-        this.imags = imgs 
-        this.buttons = buttons
-        this.disactImgClass = imgDisactiveClass
-        this.disactBtnClass =  buttonDisactiveClass
+  init() {
+    const { arrowPrev } = this;
+    const { arrowNext } = this;
 
+    const imgs = this.imags;
+    const { buttons } = this;
+    const disactImg = this.disactImgClass;
+    const disactBtn = this.disactBtnClass;
+
+    let currentIndex = 0;
+    const { length } = imgs;
+
+    arrowPrev.addEventListener('click', handlerArrowPrevClick);
+    arrowNext.addEventListener('click', handlerArrowNextClick);
+    /// ////
+    function handlerArrowPrevClick() {
+      let newIndex = currentIndex - 1;
+      if (newIndex < 0) newIndex = length - 1;
+
+      imgs.forEach((image, iIndex) => {
+        if (iIndex === newIndex) {
+          image.classList.remove(disactImg);
+          buttons[iIndex].classList.remove(disactBtn);
+        } else {
+          image.classList.add(disactImg);
+          buttons[iIndex].classList.add(disactBtn);
+        }
+      });
+
+      currentIndex = newIndex;
     }
 
-    init(){
-        let arrowPrev = this.arrowPrev 
-        let arrowNext = this.arrowNext
-        
-        let imgs = this.imags 
-        let buttons = this.buttons
-        let disactImg  = this.disactImgClass
-        let disactBtn = this.disactBtnClass
+    function handlerArrowNextClick() {
+      let newIndex = currentIndex + 1;
+      if (newIndex >= length) newIndex = 0;
 
-        let parent = arrowPrev.parentNode
-        let currentIndex = 0 
-        let length = imgs.length
+      imgs.forEach((image, iIndex) => {
+        if (iIndex === newIndex) {
+          image.classList.remove(disactImg);
+          buttons[iIndex].classList.remove(disactBtn);
+        } else {
+          image.classList.add(disactImg);
+          buttons[iIndex].classList.add(disactBtn);
+        }
+      });
 
-        arrowPrev.addEventListener("click", handlerArrowPrevClick)
-        arrowNext.addEventListener("click", handlerArrowNextClick)
-        ///////
-        function handlerArrowPrevClick(e){
-            let newIndex = currentIndex - 1 
-            if(newIndex<0) newIndex = length-1 
-    
-            imgs.forEach((image, iIndex)=>{
-                if(iIndex == newIndex){
-                    image.classList.remove(disactImg)
-                    buttons[iIndex].classList.remove(disactBtn)
-                }
-                else{
-                    image.classList.add(disactImg)
-                    buttons[iIndex].classList.add(disactBtn)
-                }
-            })
-    
-            currentIndex = newIndex
-        }
-    
-        function handlerArrowNextClick(e){
-            let newIndex = currentIndex + 1 
-            if(newIndex>=length) newIndex = 0
-    
-            imgs.forEach((image, iIndex)=>{
-                if(iIndex == newIndex){
-                    image.classList.remove(disactImg)
-                    buttons[iIndex].classList.remove(disactBtn)
-                }
-                else{
-                    image.classList.add(disactImg)
-                    buttons[iIndex].classList.add(disactBtn)
-                }
-            })
-            
-            currentIndex = newIndex
-        }
+      currentIndex = newIndex;
     }
+  }
 }
 
-export default arrowImgsSlider
-
+export default arrowImgsSlider;
--- src/plugins/buttons-imgs-slider.js
+++ src/plugins/buttons-imgs-slider.js
@@ -1,43 +1,39 @@
-function buttonsImgsSlider(data){
-    let slider = new ButtonsImgsSlider(data)
-    slider.init()
-    return slider
+function buttonsImgsSlider(data) {
+  const slider = new ButtonsImgsSlider(data);
+  slider.init();
+  return slider;
 }
 
-class ButtonsImgsSlider{
+class ButtonsImgsSlider {
+  constructor({
+    buttons, imgs, buttonDisactiveClass, imgDisactiveClass,
+  }) {
+    this.buttons = buttons;
+    this.imags = imgs;
+    this.disactImgClass = imgDisactiveClass;
+    this.disactBtnClass = buttonDisactiveClass;
+  }
 
-    constructor({ buttons, imgs, buttonDisactiveClass, imgDisactiveClass }){
-        this.buttons = buttons
-        this.imags = imgs 
-        this.disactImgClass = imgDisactiveClass
-        this.disactBtnClass =  buttonDisactiveClass
-    }
-    init(){
-        let imgs = this.imags 
-        let buttons = this.buttons
-        let imgDisactiveClass  = this.disactImgClass
-        let buttonDisactiveClass = this.disactBtnClass
+  init() {
+    const imgs = this.imags;
+    const { buttons } = this;
+    const imgDisactiveClass = this.disactImgClass;
+    const buttonDisactiveClass = this.disactBtnClass;
 
-        buttons.forEach((button, bIndex) => {
-            button.addEventListener("mouseover", buttonHoverListener)
-            function buttonHoverListener(event){
-        
-                imgs.forEach((image, iIndex)=>{
-                    if(iIndex==bIndex) image.classList.remove(imgDisactiveClass)
-                    else image.classList.add(imgDisactiveClass)
-                })
-                buttons.forEach((el)=>{
-                    el.classList.add(buttonDisactiveClass)
-                })
-                button.classList.remove(buttonDisactiveClass)
-            } 
-    
+    buttons.forEach((button, bIndex) => {
+      button.addEventListener('mouseover', buttonHoverListener);
+      function buttonHoverListener() {
+        imgs.forEach((image, iIndex) => {
+          if (iIndex === bIndex) image.classList.remove(imgDisactiveClass);
+          else image.classList.add(imgDisactiveClass);
         });
-    }
-    
+        buttons.forEach((el) => {
+          el.classList.add(buttonDisactiveClass);
+        });
+        button.classList.remove(buttonDisactiveClass);
+      }
+    });
+  }
 }
 
-
-
-
-export default buttonsImgsSlider
+export default buttonsImgsSlider;
