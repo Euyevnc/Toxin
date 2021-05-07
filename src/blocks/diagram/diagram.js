0c523974769c3e7684dcdf45fb4459502b6193c2
Euyevnc <remarka234@gmail.com>
2021-05-07 10:26:55 +0700

    #30. Диаграмма работает как должна

--- src/blocks/diagram/diagram.js
+++ src/blocks/diagram/diagram.js
@@ -4,73 +4,35 @@ const firstElement = document.querySelector('.js-diagram');
 class Diagram {
   constructor(root = firstElement) {
     this.root = root;
-    this.canvas = this.root.querySelector('.js-diagram__canvas');
-    this.subjectForm = Object.values(this.root.querySelector('.js-diagram__reviews-amount').dataset);
+    this.canvas = root.querySelector('.js-diagram__canvas');
+    this.subjectForms = Object.values(root.querySelector('.js-diagram__reviews-amount').dataset);
+
+    this.legendItems = root.querySelectorAll('.diagram__legend-item');
+    this.sections = [...this.legendItems].map((item) => new DiagramSection(item));
 
     this.#init();
   }
 
   #init = () => {
-    const { root, canvas, subjectForm } = this;
-
+    const { canvas } = this;
     const ctxM = canvas.getContext('2d');
+    const amounts = this.sections.map((item) => item.amount);
+    const names = this.sections.map((item) => item.name);
+    const gradients = this.sections.map((item) => this.#createGradient(ctxM, item.colors));
 
-    const yellowGrad = ctxM.createLinearGradient(155, 0, 155, 120);
-    yellowGrad.addColorStop(0, '#FFE39C');
-    yellowGrad.addColorStop(1, '#FFBA9C');
+    const total = this.sections.reduce((prev, cur) => prev + cur.amount, 0);
+    this.#fillTheLabel(total);
 
-    const greenGrad = ctxM.createLinearGradient(155, 0, 155, 120);
-    greenGrad.addColorStop(0, '#6FCF97');
-    greenGrad.addColorStop(1, '#66D2EA');
-
-    const purpleGrad = ctxM.createLinearGradient(155, 0, 155, 120);
-    purpleGrad.addColorStop(0, '#BC9CFF');
-    purpleGrad.addColorStop(1, '#8BA4F9');
-
-    const blackGrad = ctxM.createLinearGradient(155, 0, 155, 120);
-    blackGrad.addColorStop(0, '#919191');
-    blackGrad.addColorStop(1, '#3D4975');
-
-    const amountArray = [];
-    const nameArray = [];
-    let total = 0;
-
-    root.querySelectorAll('.diagram__legend-item').forEach((item) => {
-      const amount = +item.dataset.amount;
-      const name = item.querySelector('.diagram__legend-lettering').textContent;
-      amountArray.unshift(amount);
-      nameArray.unshift(name);
-      total += amount;
-    });
-
-    let title;
-
-    if (total === 1 || total % 10 === 1) [title] = subjectForm;
-    else if (total > 4 || total % 10 > 4) [, , title] = subjectForm;
-    else [, title] = subjectForm;
-    root.querySelector('.js-diagram__subtitle').textContent = title;
-
-    root.querySelector('.js-diagram__reviews-amount').textContent = total;
     const myChart = new Chart(canvas, {
       type: 'doughnut',
       data: {
-        labels: nameArray,
+        labels: names.reverse(),
         datasets: [{
 
           label: '# of Votes',
-          data: amountArray,
-          backgroundColor: [
-            blackGrad,
-            purpleGrad,
-            greenGrad,
-            yellowGrad,
-          ],
-          borderColor: [
-            'white',
-            'white',
-            'white',
-            'white',
-          ],
+          data: amounts.reverse(),
+          backgroundColor: gradients.reverse(),
+          borderColor: Array(gradients.length).fill('white'),
           borderWidth: 1,
         }],
       },
@@ -81,7 +43,37 @@ class Diagram {
         },
       },
     });
-    this.diogram = myChart;
+    this.diagram = myChart;
+  }
+
+  #createGradient = (context, colors) => {
+    const newGrad = context.createLinearGradient(155, 0, 155, 120);
+    newGrad.addColorStop(0, colors[0]);
+    newGrad.addColorStop(1, colors[1]);
+    return newGrad;
+  }
+
+  #fillTheLabel = (total) => {
+    let title;
+    if (total === 1 || total % 10 === 1) [title] = this.subjectForms;
+    else if (total > 4 || total % 10 > 4) [, , title] = this.subjectForms;
+    else [, title] = this.subjectForms;
+    this.root.querySelector('.js-diagram__reviews-amount').textContent = total;
+    this.root.querySelector('.js-diagram__subtitle').textContent = title;
+  }
+}
+
+class DiagramSection {
+  constructor(root) {
+    this.root = root;
+    this.circle = root.querySelector('.diagram__legend-circle');
+    this.plate = root.querySelector('.diagram__legend-lettering');
+    this.amount = Number(root.dataset.amount);
+    this.name = this.plate.dataset.name;
+    this.colors = this.circle.dataset.colors.split(', ');
+
+    this.plate.textContent = this.name;
+    this.circle.style.background = `linear-gradient(180deg, ${this.colors[0]} 0%, ${this.colors[1]} 100%)`;
   }
 }
 
